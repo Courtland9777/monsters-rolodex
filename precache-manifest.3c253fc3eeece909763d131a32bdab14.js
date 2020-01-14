6d4c25f2903228b45268e50a43f9a11300db1c28
Courtland <45576496+Courtland9777@users.noreply.github.com>
2020-01-13 22:13:51 -0500

    Updates

---
+++ precache-manifest.3c253fc3eeece909763d131a32bdab14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cf8f9cf7c6ab41c6cd17ce9220c7fb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1a493a47b20e34d32a9e",
+    "url": "/monsters-rolodex/static/css/main.c43e3003.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a493a47b20e34d32a9e",
+    "url": "/monsters-rolodex/static/js/main.abfd91c2.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
